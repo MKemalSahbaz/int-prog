3519264b5b712c7f52f61ee33999f341c55a6536
MKemalSahbaz <sahbazkemal3834@gmail.com>
2025-05-24 11:08:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -256,5 +256,6 @@ def personel_sil(personel_id):
     flash('Personel silindi!')
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+import os
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
